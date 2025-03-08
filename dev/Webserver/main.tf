8ffc960b371e89feb01f5a226bcb12cb823cb2dc
EC2 Default User <ec2-user@ip-172-31-82-92.ec2.internal>
2025-03-08 02:59:19 +0000

    added volume

--- dev/Webserver/main.tf
+++ dev/Webserver/main.tf
@@ -37,6 +37,10 @@ resource "aws_instance" "ec2" {
   subnet_id                   = data.terraform_remote_state.network.outputs.public_subnet_ids
   security_groups             = [aws_security_group.security-group.id]
   associate_public_ip_address = true
+  root_block_device {
+    volume_size=50
+    volume_type="gp2"
+  }
   
   root_block_device {
     volume_size = 50
